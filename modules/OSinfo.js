91a9bc8828e96b546b1049813ce35c425624c55c
Ilona Pastucha <ilonka.p4@gmail.com>
2018-09-07 06:34:09 +0200

    Fix bugs

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -11,11 +11,12 @@ function getOSinfo() {
     var release = os.release();
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
+    var convTime = time(uptime);
     var userInfo = os.userInfo();
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('Uptime: ~', convTime);
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
